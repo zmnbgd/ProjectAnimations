927be52902cb004a9c8b00ccf39b1ecf5d182958
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2022-06-05 00:04:03 +0200

    Add files via upload

--- CustomizingAnimationsSwiftUI.swift
+++ CustomizingAnimationsSwiftUI.swift
@@ -13,23 +13,34 @@ struct CustomizingAnimationsSwiftUI: View {
     
     var body: some View {
         Button("Tap me") {
-           animationAmount += 1
+           //animationAmount += 1
         }
         .padding(50)
         .background(.orange)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
+        .overlay(
+        Circle()
+            .stroke(.orange)
+            .scaleEffect(animationAmount)
+            .opacity(2 - animationAmount)
+            .animation(
+                .easeInOut(duration: 1.0).repeatForever(autoreverses: false),
+                value: animationAmount
+            )
+        )
+        
+        .onAppear {
+            animationAmount = 2
+        }
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
 //        .animation(.interpolatingSpring(stiffness: 50.0, damping: 1.0), value: animationAmount)
-        .animation(.easeInOut(duration: 2.0), value: animationAmount)
+    }
 }
 struct CustomizingAnimationsSwiftUI_Previews: PreviewProvider {
     static var previews: some View {
         CustomizingAnimationsSwiftUI()
     }
   }
-}
 
-
-//3:16
