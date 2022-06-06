1008bbd507d7eeeb3fcee2fbd5bb865e087c2abc
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2022-06-06 23:33:10 +0200

    Add files via upload

---
+++ AnimatingGestures.swift
@@ -0,0 +1,20 @@
+//
+//  AnimatingGestures.swift
+//  Animations
+//
+//  Created by Marko Zivanovic on 6.6.22..
+//
+
+import SwiftUI
+
+struct AnimatingGestures: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimatingGestures_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimatingGestures()
+    }
+}
